04a2e2c5db88447ba77155d95d6c18322e049ab9
純平 永尾 <j-nagao@oitadensi.co.jp>
2025-04-30 10:40:13 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfHelper/CustomControls/EGrid.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfHelper.CustomControls;
+/// <summary>
+/// グリッドのカスタムコントロール
+/// </summary>
+public class EGrid : Grid
+{
+
+    private static readonly DependencyProperty ColumnsProperty =
+        DependencyProperty.Register("Columns",
+            typeof(string),
+            typeof(EGrid),
+            new PropertyMetadata(string.Empty, OnColumnsChanged));
+
+    /// <summary>
+    /// カラム定義プロパティ
+    /// </summary>
+    public string Columns
+    {
+        get
+        {
+            return (string)GetValue(ColumnsProperty);
+        }
+        set
+        {
+            SetValue(ColumnsProperty, value);
+        }
+    }
+
+    private static readonly DependencyProperty RowsProperty =
+        DependencyProperty.Register(
+            "Rows",
+            typeof(string),
+            typeof(EGrid),
+            new PropertyMetadata(string.Empty, OnRowsChanged));
+
+    /// <summary>
+    /// ロウ定義プロパティ
+    /// </summary>
+    public string Rows
+    {
+        get
+        {
+            return (string)GetValue(RowsProperty);
+        }
+        set
+        {
+            SetValue(RowsProperty, value);
+        }
+    }
+
+
+    /// <summary>
+    /// Columns変更時の処理
+    /// </summary>
+    /// <param name="d"></param>
+    /// <param name="e"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    private static void OnColumnsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // Grid取得
+        var grid = d as EGrid;
+        if (grid == null)
+        {
+            return;
+        }
+
+        // カラム定義取得
+        var strArray = d.GetValue(ColumnsProperty) as string;
+        if (strArray == null)
+        {
+            return;
+        }
+
+        // 文字列からGridLengthへのコンバータ
+        var converter = new GridLengthConverter();
+
+        // カラム定義をGridに設定
+        grid.ColumnDefinitions.Clear();
+        foreach (var item in strArray.Split(","))
+        {
+            // 文字列をGridLengthに変換
+#pragma warning disable CS8605 // null の可能性がある値をボックス化解除しています。
+            grid.ColumnDefinitions.Add(value: new ColumnDefinition() { Width = (GridLength)converter.ConvertFromString(item) });
+#pragma warning restore CS8605 // null の可能性がある値をボックス化解除しています。
+        }
+
+        // レイアウトを更新
+        grid.UpdateLayout();
+    }
+
+
+    /// <summary>
+    /// Rows変更時の処理
+    /// </summary>
+    /// <param name="d"></param>
+    /// <param name="e"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    private static void OnRowsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // Grid取得
+        var grid = d as EGrid;
+        if (grid == null)
+        {
+            return;
+        }
+
+        // ロウ定義取得
+        var strArray = d.GetValue(RowsProperty) as string;
+        if (strArray == null)
+        {
+            return;
+        }
+
+        // カラム定義をGridに設定
+        GridLengthConverter converter = new GridLengthConverter();
+
+        // ロウ定義をGridに設定
+        grid.RowDefinitions.Clear();
+        foreach (var item in strArray.Split(","))
+        {
+            // 文字列をGridLengthに変換
+#pragma warning disable CS8605 // null の可能性がある値をボックス化解除しています。
+            grid.RowDefinitions.Add(new RowDefinition() { Height = (GridLength)converter.ConvertFromString(item) });
+#pragma warning restore CS8605 // null の可能性がある値をボックス化解除しています。
+        }
+
+        // レイアウトを更新
+        grid.UpdateLayout();
+    }
+}
---
+++ WpfHelper/ViewModels/ReactiveViewModelBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using R3;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace WpfHelper.ViewModels;
+/// <summary>
+/// ReactivePropertyのViewModelのベースとなるクラス
+/// </summary>
+public class ReactiveViewModelBase : INotifyPropertyChanged, IDisposable
+{
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public ReactiveViewModelBase()
+    {
+        AddToDisposable();
+    }
+
+#pragma warning disable CS0067 // イベント 'SearchViewModel.PropertyChanged' は使用されていません
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    private CompositeDisposable disposable = new CompositeDisposable();
+    // 一括でReactivePropertyのプロパティをdisposableに追加
+    private void AddToDisposable()
+    {
+        var reactiveProperties = GetType()
+        .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+        .Where(p => p.PropertyType.IsGenericType &&
+                    (p.PropertyType.GetGenericTypeDefinition() == typeof(BindableReactiveProperty<>) ||
+                     p.PropertyType.GetGenericTypeDefinition() == typeof(ReactiveCommand<Unit>)));
+
+        // 各ReactiveプロパティをCompositeDisposableに追加
+        foreach (var property in reactiveProperties)
+        {
+            var reactiveObject = property.GetValue(this);
+            if (reactiveObject is IDisposable ro)
+            {
+                ro.AddTo(disposable);
+            }
+        }
+    }
+    public void Dispose()
+    {
+        BeforeDispose();
+        disposable.Dispose();
+    }
+
+
+    protected virtual void BeforeDispose() { }
+}
+
