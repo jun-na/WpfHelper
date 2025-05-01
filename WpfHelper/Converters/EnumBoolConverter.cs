c5d7098199c94a468baabed5152e936e433ec64c
jun-na <junpei500rr@gmail.com>
2025-05-01 13:10:22 +0900

    利用者一覧実装中

---
+++ WpfHelper/Converters/EnumBoolConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfHelper.Converters;
+
+/// <summary>
+/// Enumの状態によりtrue/falseを返すConverter
+/// </summary>
+[ValueConversion(typeof(Enum), typeof(bool))]
+public class EnumBoolConverter : IValueConverter
+{
+    /// <summary>
+    /// ViewModelからXAMLに値を渡すときに呼ばれる
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null || parameter == null)
+        {
+            return false;
+        }
+        if (value.ToString() == parameter.ToString())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// XAMLからViewModelに値を渡すときに呼ばれる
+    /// </summary>  
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ WpfHelper/Extensions/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.ObjectModel
+{
+    public static class CollectionExtensions
+    {
+        public static Collection<T> AddRange<T>(this Collection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                collection.Add(item);
+            }
+            return collection;
+        }
+    }
+}
