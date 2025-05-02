e53620320184f151af68b75dff0c106359aec294
jun-na <junpei500rr@gmail.com>
2025-05-02 15:18:59 +0900

    単価の取り込みを実装

--- WpfHelper/Extensions/CollectionExtensions.cs
+++ WpfHelper/Extensions/CollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,49 @@ namespace System.Collections.ObjectModel
 {
     public static class CollectionExtensions
     {
+        /// <summary>
+        /// コレクションを追加
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
         public static Collection<T> AddRange<T>(this Collection<T> collection, IEnumerable<T> items)
         {
+            foreach (var item in items)
+            {
+                collection.Add(item);
+            }
+            return collection;
+        }
+
+        /// <summary>
+        /// 値が異なればクリアして設定
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static Collection<T> SetRange<T>(this Collection<T> collection, IEnumerable<T> items, Func<T, T, bool>? equalFunc = null)
+        {
+            if (equalFunc != null && collection.Count() == items.Count())
+            {
+                var isNotEqual = false;
+                for (var i = 0; i < collection.Count; i++)
+                {
+                    if (!equalFunc(collection[i], items.ElementAt(i)))
+                    {
+                        isNotEqual = true;
+                        break;
+                    }
+                }
+                if (!isNotEqual)
+                {
+                    return collection;
+                }
+            }
+
+            collection.Clear();
             foreach (var item in items)
             {
                 collection.Add(item);
